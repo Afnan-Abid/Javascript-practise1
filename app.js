b315eda31a8994e1c626491df2e2538bd8984ff0
Afnan-Abid <afnanabid765@gmail.com>
2023-10-27 15:04:42 -0700

    full work here

---
+++ app.js
@@ -0,0 +1,498 @@
+
+/*Question 1
+
+
+display your Name using alert
+alert("Muhammad rafay")*/
+
+
+/* Question 2
+
+display your text using line breaks to break lines
+alert("Hello world \n Pakistan zindabad")*/
+
+/* Question 3 
+
+display your text in console using alert
+  console.log("Hello world cricket");
+  alert("hello world cricket");*/
+
+
+
+
+
+  /*Question 1
+
+var userName */
+
+
+
+ /*Question 2 declare variable and assign in string
+
+
+ var myName = "Muhammad Rafay"*/
+
+
+/*Question 3 declare variable and assign & display in alert
+
+
+var message = "hello world"
+
+alert(message)*/
+
+
+
+/*Question 4
+
+
+var names = "Muhammad rafay"
+var age = +"23"
+var education = "BCOM"
+var job = "Salesman"
+
+
+alert(names)
+alert(age)
+alert(education)
+alert(job)*/
+
+
+
+/*Question 5
+
+/var food = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+alert(food)*/
+
+
+
+ /*Question 6 
+
+
+
+ let children = "with 2 kids";
+ let partnersName = " and married to Abc";
+ let location = " in England";
+ let job = "you will be a Software developer";
+ 
+ alert(job + location + partnersName + " " + children);
+ 
+ 
+ not working*/
+ 
+ /*Question 7
+
+
+
+
+
+ var email = "abc2@gmail.com"
+ alert("my email address is"  + email)*/
+
+
+
+/*Question 8
+
+
+var book = "A smarter way to learn javascript"
+alert("I am trying to learn from the book "   + book)*/
+
+
+
+
+
+/*Question 9
+
+
+
+
+
+var names = " My Name is Muhammad rafay"
+var age = " I Am 23 years old"
+var education = " I Study In BCOM"
+var job = " I take mobile and cloud computing Module A class"
+
+
+alert(names)
+alert(age)
+alert(education)
+alert(job)*/
+
+
+
+
+/*Question 10
+
+
+
+var para = document.write("Yay! I can write HTML content through Javascript")*/
+
+
+
+
+  /* 3 Variables for numbers*/
+
+
+
+
+/*Question 1 
+
+
+
+
+let age = alert("I am 21 years old")*/
+
+
+
+
+/*Question 2 
+
+
+let vistedTimes = alert("You have visited this site 20 times ")*/
+
+
+
+
+/*Question 3
+
+
+let birthYear = document.write("My birth year is 2000") */
+
+
+
+/* Question 4
+
+
+
+let vistorsName = "John Doe"
+let productTitle = "XYZ Clothing store"
+let quantity = "10 Tshirts"
+
+document.write(vistorsName  + " " + " " + "Ordered" + " "  +  quantity  + " " + " " + "on" + " " +  productTitle )*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+
+
+  
